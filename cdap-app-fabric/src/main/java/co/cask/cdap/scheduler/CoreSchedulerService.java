0fcac3f602e873deead7f02c46d9cac0574274ee
Chengfeng <mao@cask.co>
2017-10-19 15:27:56 -0700

    remove unused imports

--- cdap-app-fabric/src/main/java/co/cask/cdap/scheduler/CoreSchedulerService.java
+++ cdap-app-fabric/src/main/java/co/cask/cdap/scheduler/CoreSchedulerService.java
@@ -66,6 +66,8 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
